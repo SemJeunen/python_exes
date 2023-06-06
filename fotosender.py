b5c5c3b802488f6e68745919b83cf3411fafd2e7
Sem Jeunen <sem.jeunen@gmail.com>
2023-06-06 12:20:41 +0200

    fixed fotosender

--- fotosender.py
+++ fotosender.py
@@ -1,4 +1,4 @@
-import os, cv2, datetime
+import os, cv2, datetime,time
 
 cam = cv2.VideoCapture(0)
 if os.path.exists("images") != True:
@@ -7,6 +7,8 @@ if os.path.exists("images") != True:
 for i in range(0,10):
     ret, frame = cam.read()
     cv2.imwrite(f"images/{datetime.date.today()}-{i}.png", frame)
+    time.sleep(60)
+
 
 os.system("git add .")
 os.system("git commit -m 'images-result'")
